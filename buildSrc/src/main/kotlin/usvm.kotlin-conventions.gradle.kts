60f01486a0b308dd269ded43c077799bd2fcfafc
Danil P <58734659+ancavar@users.noreply.github.com>
2024-05-15 19:49:23 +0300

    Add oracle to path selector

--- buildSrc/src/main/kotlin/usvm.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/usvm.kotlin-conventions.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
+    implementation("com.microsoft.onnxruntime:onnxruntime:${Versions.onnxruntime}")
 
     testImplementation(kotlin("test"))
 
