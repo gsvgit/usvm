5a0c859ec20a9282227117253dad1161ff69a9d5
Danil P <58734659+ancavar@users.noreply.github.com>
2024-05-23 15:07:31 +0300

    Add `methodOf` for BlockGraph to support adding blocks to coverage zone

--- usvm-core/src/main/kotlin/org/usvm/statistics/BlockGraph.kt
+++ usvm-core/src/main/kotlin/org/usvm/statistics/BlockGraph.kt
@@ -1,6 +1,6 @@
 package org.usvm.statistics
 
-interface BlockGraph<Block, Statement> {
+interface BlockGraph<Method, Block, Statement> {
     fun predecessors(node: Block): Sequence<Block>
     fun successors(node: Block): Sequence<Block>
 
@@ -9,6 +9,8 @@ interface BlockGraph<Block, Statement> {
 
     fun returnOf(node: Block): Sequence<Block>
 
+
+    fun methodOf(block: Block): Method
     fun blockOf(inst: Statement): Block
     fun statementsOf(node: Block): Sequence<Statement>
 }
